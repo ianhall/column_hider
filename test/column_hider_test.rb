8947c1c1a242675991e1f4e38a8c9edac1c2cf6c
Ian Hall <ianh.99@gmail.com>
2015-05-20 16:33:10 -0700

    get tests to run

--- test/column_hider_test.rb
+++ test/column_hider_test.rb
@@ -1,69 +1,53 @@
+require 'minitest/autorun'
+require 'minitest/spec'
 require 'test_helper'
 
-class ColumnHiderTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, ColumnHider
+describe ColumnHider do
+  it 'has a version number' do
+    expect(ColumnHider::VERSION).wont_be_nil
+  end
+
+  before do
+    class Country < ActiveRecord::Base
+      establish_connection :adapter => 'sqlite3', :database => ':memory:'
+      connection.create_table(:countries, :force => true) do |t|
+        t.string :name
+        t.string :capital
+        t.string :comment
+      end
+      extend ColumnHider::ActiveRecordAttributes
+      hidden_columns :capital
+    end
+  end
+
+  after do
+    Object.send :remove_const, :Country
+  end
+
+  describe '#column is removed in the model' do
+    let(:mock_col_arr) { %w(id name comment) }
+    col_arr = []
+    it '#shows the column is not there' do
+      Country.columns.each do |col|
+        col_arr << col.name
+      end
+      expect(col_arr).must_equal(mock_col_arr)
+      expect(col_arr).must_include('comment')
+      expect(col_arr).wont_include('capital')
+    end
+  end
+
+  describe '#column is removed dynamically' do
+    let(:mock_col_arr) { %w(id name capital) }
+    col_arr = []
+    it '#shows the column is not there' do
+      Country.send :hidden_columns, :comment
+      Country.columns.each do |col|
+        col_arr << col.name
+      end
+      expect(col_arr).must_equal(mock_col_arr)
+      expect(col_arr).must_include('capital')
+      expect(col_arr).wont_include('comment')
+    end
   end
 end
-
-
-
-# require 'test/test_helper'
-#
-# describe ColumnHider do
-#   it 'has a version number' do
-#     expect(ColumnHider::VERSION).not_to be nil
-#   end
-#
-# end
-#
-# describe ActiveRecord::Attributes, "active record attributes" do
-#
-#   before do
-#     class Country < ActiveRecord::Base
-#       establish_connection :adapter => "sqlite3", :database => ":memory:"
-#       connection.create_table(:countries, :force => true) do |t|
-#         t.string :name
-#         t.string :capital
-#         t.string :comment
-#       end
-#       extend Columns::ActiveRecordAttributes
-#       removed_columns :capital
-#     end
-#   end
-#
-#   after do
-#     Object.send :remove_const, :Country
-#   end
-#
-#   describe "#columns" do
-#     context 'column is removed in the model' do
-#       let(:mock_col_arr) { %w(id name comment) }
-#       it "shows the removed column is not present in the table" do
-#         col_arr = []
-#         Country.columns.each do |col|
-#           col_arr << col.name
-#         end
-#         expect(col_arr).to eq(mock_col_arr)
-#         expect(col_arr).to include('comment')
-#         expect(col_arr).not_to include('capital')
-#       end
-#     end
-#
-#     context 'column is removed dynamically' do
-#       let(:mock_col_arr) { %w(id name capital) }
-#       before do
-#         Country.send :removed_columns, :comment
-#       end
-#       it "shows a previously removed column can be restored, and a different column removed" do
-#         col_arr = []
-#         Country.columns.each do |col|
-#           col_arr << col.name
-#         end
-#         expect(col_arr).to eq(mock_col_arr)
-#         expect(col_arr).to include('capital')
-#         expect(col_arr).not_to include('comment')
-#       end
-#     end
-#   end
-# end
